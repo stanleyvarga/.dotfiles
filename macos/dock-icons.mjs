76352d1b02d884f35f806d289244081155d5d496
Stanley Varga <stanislav.varga@brackets.sk>
2023-03-27 00:02:15 +0200

    ♻️ refactor(dock-icons)

--- macos/dock-icons.mjs
+++ macos/dock-icons.mjs
@@ -11,12 +11,16 @@ const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 const intersect = (a, b) => [...new Set(a)].filter(x => new Set(b).has(x));
 const replaceBy = (search, replacement) => (str) => str.replace(search, replacement)
 const splitBy = (delimiter) => (str) => str.split(delimiter)
+const splitByNewLine = splitBy('\n')
 const map = (fn) => (arr) => arr.map(fn)
 const filter = (fn) => (arr) => arr.filter(fn)
+const isEmptyString = (str) => str !== ''
+const toString = (unknown) => unknown.toString()
 
-const parseListDirectories = pipe(
-		splitBy('\n'),
-		filter((dir) => dir !== ''),
+const parseDirList = pipe(
+		toString,
+		splitByNewLine,
+		filter(isEmptyString),
 		map((dir) => dir.replace(PATH_ICONS, '').replace(PATH_APPLICATIONS, '').replace(/\//g, '').replace('.app', '')),
 )
 
@@ -31,11 +35,12 @@ const setFileIcon = async (app) => {
 
 void async function () {
 	let iconDirectories = await $`ls -d ${PATH_ICONS}/*/`
-	let AppDirectories = await $`ls -d ${PATH_APPLICATIONS}/*/`
-	
-	const appsHavingIcons = intersect(parseListDirectories(iconDirectories.toString()), parseListDirectories(AppDirectories.toString()))
+	let appDirectories = await $`ls -d ${PATH_APPLICATIONS}/*/`
 
-	appsHavingIcons.forEach(setFileIcon)
+	iconDirectories = parseDirList(iconDirectories)
+	appDirectories = parseDirList(appDirectories)
+	
+	intersect(iconDirectories, appDirectories).forEach(setFileIcon)
 
 	// ♻️ Refresh Icon cache, otherwise icons will not show up
 	// Wait to ensure icons have been set
