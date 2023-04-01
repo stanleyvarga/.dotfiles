ef6347a63495cf8264ce6cd52fbc77c179fc8bf2
Stanley Varga <stanislav.varga@brackets.sk>
2023-04-01 14:42:05 +0200

    ♻️ ref(zsh): scripts

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -1,1 +1,6 @@
 zsh -c "cd yarn && chmod +x ./yarn.global.sh"
+
+# eval "$(oh-my-posh init zsh --config $DOTFILES/oh-my-posh/amro.omp.json)"
+eval "$(oh-my-posh init zsh --config $DOTFILES/oh-my-posh/robbyrussel.omp.json)"
+eval "$(zoxide init zsh)"
+eval "$(atuin init zsh)"
